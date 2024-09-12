e2bd8446be3259d92328d588a2cabfdfe1c93506
ES <23021423@uepg.br>
2024-09-12 16:33:37 -0300

    mistake

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-    proxy: {
-        '/api': {
-        target: 'https://backend-massante.onrender.com',
-        changeOrigin: true,
-        secure: false,
+        proxy: {
+            '/api': {
+            target: 'https://backend-massante.onrender.com',
+            changeOrigin: true,
+            secure: false,
+            }
         }
-    }
+    },
+    build: {
+        outDir: 'build'
     }
 })
